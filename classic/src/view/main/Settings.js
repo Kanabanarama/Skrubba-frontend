4241c735d8a115e5817aa10bc9491126e3685eb9
kana <kanabanarama@googlemail.com>
2016-01-15 18:00:55 +0100

    Added User credentials configuration in settings panel

--- classic/src/view/main/Settings.js
+++ classic/src/view/main/Settings.js
@@ -12,7 +12,7 @@ Ext.define('Skrubba.view.main.Settings', {
 
     layout: 'border',
 
-    height: 400,
+    height: 450,
     bodyPadding: 10,
 
     defaults: {
@@ -25,6 +25,8 @@ Ext.define('Skrubba.view.main.Settings', {
         title: 'Valves',
         region:'west',
         width: 300,
+        height: 200,
+        collapsible: false,
         items: [
         {
             xtype:'combo',
@@ -44,18 +46,84 @@ Ext.define('Skrubba.view.main.Settings', {
             editable: false,
             emptyText: 'How many valves are installed?'
         }],
-        html: '<p>This setting prevents that more valves can be added and configured than are physically present.</p>'
+        html: '<p>This setting prevents that more valves can be added and configured than are physically present.</p>',
+        buttons: [{
+            text: 'Save',
+            handler: 'onValveAmountSave'
+        }]
     },
     {
         title: 'User',
         region: 'center',
-        html: '<p>TODO</p>'
+        height: 200,
+        collapsible: false,
+        items: [
+            {
+                xtype: 'fieldcontainer',
+                layout: 'hbox',
+                items: [
+                    {
+                        xtype: 'textfield',
+                        layout: 'vbox',
+                        align: 'stretch',
+                        itemId: 'username',
+                        inputId: 'username',
+                        fieldLabel: 'Username',
+                        labelWidth: 120,
+                        allowBlank: false,
+                        msgTarget : 'side'
+                    }
+                ]},
+
+
+            {
+                xtype: 'fieldcontainer',
+                //combineErrors: true,
+                msgTarget : 'side',
+                layout: 'hbox',
+                items: [
+                    {
+                        xtype: 'textfield',
+                        inputType: 'password',
+                        fieldLabel: 'Password',
+                        labelWidth: 120,
+                        margin: '0 30 0 0',
+                        allowBlank: false,
+                        msgTarget : 'side',
+                        id: 'configSetPassword'
+                    },
+                    {
+                        xtype: 'textfield',
+                        inputType: 'password',
+                        fieldLabel: 'Confirm Password',
+                        labelWidth: 120,
+                        allowBlank: false,
+                        validationEvent: 'blur',
+                        msgTarget: 'side',
+                        id: 'configSetPasswordConfirm',
+                        validator: function() {
+                            var password = Ext.getCmp('configSetPassword').getValue();
+                            var passwordConfirm = Ext.getCmp('configSetPasswordConfirm').getValue();
+                            var validationResult = (password == passwordConfirm) ? true : 'Passwords do not match!';
+                            return validationResult;
+                        }
+                    }
+                ]
+            }
+
+        ],
+        html: '<p>It is strongly recommended to set a password for the configuration interface or anyone in your network can change the settings!</p>',
+        buttons: [{
+            text: 'Save',
+            handler: 'onCredentialsSave'
+        }]
     },
     {
         title: 'About',
         region: 'south',
         height: 100,
-        minHeight: 75,
+        collapsible: false,
+        //minHeight: 75,
         html: '<p>TODO</p>'
     }],
 
